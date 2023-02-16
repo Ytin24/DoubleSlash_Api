b9f914562429d9546f3bee6ef87c6519e49d952e
Ytin24 <lepaew228@gmail.com>
2023-02-16 14:25:44 +0300

    Добавьте файлы проекта.

---
+++ DoubleSlash_Api/Controllers/GetCpu.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoubleSlash_Api.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetCpu : ControllerBase {
+        [HttpGet()]
+        public string GetAll(string a) {
+            return a;
+        }
+    }
+}
